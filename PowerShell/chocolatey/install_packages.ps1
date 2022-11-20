c6cd49032ace97dad35e0ac151619b9529f43ca9
Leonardo Carbone <leonardoz.carbone@gmail.com>
2022-11-20 14:21:41 +0000

    Chocolatey

--- PowerShell/chocolatey/install_packages.ps1
+++
@@ -1,6 +0,0 @@
-$env:Path += ";C:\ProgramData\chocolatey\bin"
-
-Write-Host "[INFO] ... Installing Packages"
-choco feature enable -n allowGlobalConfirmation
-choco install packages.config
-
---
+++ PowerShell/install_chocolatey.ps1
@@ -0,0 +1,31 @@
+# Set-ExecutionPolicy Bypass -Scope Process -Force; 
+# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+Start-Transcript -Path "install_chocolatey.log" -Force
+
+$env:Path += ";C:\ProgramData\chocolatey\bin"
+
+choco feature enable -n allowGlobalConfirmation
+
+choco install speccy --ignore-checksums --params "/UseSystemLocale"
+choco install cpu-z
+choco install speedtest-by-ookla
+choco install treesizefree
+choco install paint.net
+choco install steam-client
+choco install vscode
+choco install docker-desktop --ignore-package-exit-codes
+choco install postman
+choco install 7zip
+choco install dropbox
+choco install firefox
+choco install foxitreader
+choco install notepadplusplus
+choco install plexmediaserver
+choco install spotify
+choco install vlc
+choco install powershell-core
+choco install powertoys
+choco install git
+
+Stop-Transcript
